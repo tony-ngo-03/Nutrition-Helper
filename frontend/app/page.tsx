dc4e2fb8345283c794d7ca597dcba2628a8fb829
tony-ngo-03 <tonyngo@utexas.edu>
2024-10-05 23:01:43 -0500

    create next app

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home(){
+  return (
+    <section>
+      <div className="container">
+        <h1 className="text-3xl font-bold">Next TS Starter</h1>
+      </div>
+    </section>
+  );
+}
